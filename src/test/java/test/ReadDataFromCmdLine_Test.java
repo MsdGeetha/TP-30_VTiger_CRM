41f69ba618342d39a5f85b017f063937bc560c00
GEETHA <GEETHA@DESKTOP-8KTIUQD>
2025-12-31 11:20:27 +0530

    commit

--- src/test/java/test/ReadDataFromCmdLine_Test.java
+++ src/test/java/test/ReadDataFromCmdLine_Test.java
@@ -1,8 +1,5 @@
 package test;
 
-import java.time.Duration;
-
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
